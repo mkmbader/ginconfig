1b247627dcabd9ce65f6990aa224fde799ff5342
Maria Bader <maria.bader@mollie.com>
2023-07-05 09:50:07 +0000

    move stuff to notebook, first examples

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pandas as pd
-import gin
+import gin #pylint:disable=import-error
+# from pydantic import StrictStr  # pylint: disable=no-name-in-module
+# type hinting: https://stackoverflow.com/questions/2489669/how-do-python-functions-handle-the-types-of-parameters-that-you-pass-in
 
 
-
-def hello_world(text:str):
-    print(text)
+@gin.configurable
+def hello_world(name:str, loc:str=gin.REQUIRED):
+    print('Hello %s, %s' %(name, loc))
     return
 
 
 if __name__ == '__main__':
 
+    gin.enter_interactive_mode() 
     gin.parse_config_file('config.gin')
 
-    #no config
-    hello_world('hello')
-    hello_world(17)
+    #no config used -> I can force these values
+    hello_world('world', 'around')
+    hello_world(17, 13)
+
+    # this uses the config values
+    hello_world()
