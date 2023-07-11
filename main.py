8c752e0877a180bd510178b423fbf4a54e514b97
Maria Bader <maria.bader@mollie.com>
2023-07-11 15:17:34 +0000

    typer

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 import gin
 import random
+import typer
 
+# allows typer commands
+app = typer.Typer()
+
+# configurables for gin
 @gin.configurable()
-def function(name = gin.REQUIRED, food = gin.REQUIRED, price = gin.REQUIRED):
+def function(name, food, price=gin.REQUIRED):
     '''print favorite food'''
-    print('%s likes %s, if cheaper than %s euro.' %(name, food, round(price,2)))
+    print('%s likes %s, if cheaper than %s.' %(name, food, round(price, 2)))
     return
 
-
+# make existing function configurable
 gin.external_configurable(random.uniform)
 
-if __name__ == '__main__':
+# example to outline gin-config functionalities
+@app.command('config')
+def gin_config_example():
+
     gin.parse_config_file('config.gin')
 
-    with gin.config_scope('maria'): function()
+    # root scope
+    function()
+
+    # sub-scope
     with gin.config_scope('ana'): function()
+    with gin.config_scope('wessel'): function()
+
+# example to outline typer functionalities
+@app.command('typer')
+def typer_example(scope:str, showroot:bool = True):
+
+    gin.parse_config_file('config.gin')
+
+    # root scope
+    if showroot:
+        function()
+  
+    # sub-scope
+    with gin.config_scope(scope): function()
+
+
+
+def cli() -> None:
+    """Helper function to start cli via poetry scripts."""
+    app()
+
+if __name__ == '__main__':
+    app()
+
+
+
